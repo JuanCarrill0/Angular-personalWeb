b72f68c53847e2691862dbe0840f374e2a51c92d
Ju4nC4rrill0 <ju4nc4rrill00@gmail.com>
2022-11-09 11:40:13 -0500

    intento host GH Pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,8 +7,6 @@ const routes: Routes = [
     loadChildren: () => import('./pages/pages.module')
       .then(m => m.PagesModule),
   },
-  { path: '', redirectTo: 'pages', pathMatch: 'full' },
-  { path: '**', redirectTo: 'pages' },
 ];
 
 const config: ExtraOptions = {
